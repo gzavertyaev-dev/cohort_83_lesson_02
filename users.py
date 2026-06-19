fdf545c04005c5c7fd19f62e92d2f1b4e1f9e74c
gzavertyaev-dev <gzavertyaev.dev@gmail.com>
2026-06-19 04:00:08 +0200

    Add files users, readme, gitignore

---
+++ users.py
@@ -0,0 +1,39 @@
+users = [
+    {
+        "firstName": "John",
+        "lastName": "Doe",
+        "age": 28,
+        "hobby": "football",
+        "job": "engineer"
+    },
+    {
+        "firstName": "Anna",
+        "lastName": "Smith",
+        "age": 24,
+        "hobby": "painting",
+        "job": "designer"
+    },
+    {
+        "firstName": "Robert",
+        "lastName": "Johnson",
+        "age": 35,
+        "hobby": "fishing",
+        "job": "driver"
+    },
+    {
+        "firstName": "Emily",
+        "lastName": "Brown",
+        "age": 31,
+        "hobby": "reading",
+        "job": "teacher"
+    },
+    {
+        "firstName": "Michael",
+        "lastName": "Davis",
+        "age": 42,
+        "hobby": "photography",
+        "job": "programmer"
+    }
+]
+
+print(users)
